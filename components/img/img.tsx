5fa7689c726ff0521ca7a281c7279d1c16a05f7e
user <progexplorer@gmail.com>
2020-06-09 10:43:34 +0200

    feat: add img component

---
+++ components/img/img.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import { SerializedStyles } from "@emotion/core";
+
+export type ImgProps = {
+  css?: SerializedStyles;
+  alt: string;
+  src: string;
+};
+
+const Img: FC<ImgProps> = ({ alt = "", src = "/", ...props }) => {
+  return <img alt={alt} src={src} {...props} />;
+};
+
+export default Img;
