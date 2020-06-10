55feff6470d5bfac780a573b685697b962d16997
user <progexplorer@gmail.com>
2020-06-10 08:42:43 +0200

    feat: create mdx page wrapper

---
+++ pages/font-size.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NextPage } from "next";
+
+type FontSizeProps = {};
+
+const FontSize: NextPage<FontSizeProps> = () => {
+  return <div>Font Size</div>;
+};
+
+export default FontSize;
