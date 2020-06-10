65f4bb85376de595cd6b493793b5aecd0a79c48a
user <progexplorer@gmail.com>
2020-06-10 13:49:34 +0200

    feat: add board component

---
+++ components/docs/board/board.tsx
@@ -0,0 +1,23 @@
+import React, { FC, ReactNode } from "react";
+import { SerializedStyles, css } from "@emotion/core";
+import tw from "twin.macro";
+
+type BoardProps = {
+  css?: SerializedStyles;
+  children: ReactNode;
+};
+
+const Board: FC<BoardProps> = ({ children, ...props }) => {
+  return (
+    <div
+      css={css`
+        ${tw`p-2 mb-6 bg-gray-100 border border-gray-400 rounded-lg shadow-lg `}
+      `}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Board;
