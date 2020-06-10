d94e1f53e8d2ba8587bdce032384fd3787c14a7f
user <progexplorer@gmail.com>
2020-06-10 08:57:42 +0200

    feat: add data.ts

---
+++ data.ts
@@ -0,0 +1,46 @@
+import tw from "twin.macro";
+
+export const defaultFontClasses = {
+  "text-xs": {
+    value: `font-size: .75rem;`,
+    twClass: tw`text-xs`,
+  },
+  "text-sm": {
+    value: `font-size: .875rem;`,
+    twClass: tw`text-sm`,
+  },
+  "text-base": {
+    value: `font-size: 1rem;`,
+    twClass: tw`text-base`,
+  },
+  "text-lg": {
+    value: `font-size: 1.125rem;`,
+    twClass: tw`text-lg`,
+  },
+  "text-xl": {
+    value: `font-size: 1.25rem;`,
+    twClass: tw`text-xl`,
+  },
+  "text-2xl": {
+    value: `font-size: 1.5rem;`,
+    twClass: tw`text-2xl`,
+  },
+  "text-3xl": {
+    value: `font-size: 1.875rem;`,
+    twClass: tw`text-3xl`,
+  },
+  "text-4xl": {
+    value: `font-size: 2.25rem;`,
+    twClass: tw`text-4xl`,
+  },
+  "text-5xl": {
+    value: `font-size: 3rem;`,
+    twClass: tw`text-5xl`,
+  },
+  "text-6xl": {
+    value: `font-size: 4rem;`,
+    twClass: tw`text-6xl`,
+  },
+};
+
+export const customFontClasses = {};
