70981268aee715f9fa9e57b29b3b3f3d18fdb3b7
user <progexplorer@gmail.com>
2020-06-10 14:10:55 +0200

    refactor: change tailwind table header

--- components/tailwind-table/tailwind-table.tsx
+++ components/tailwind-table/tailwind-table.tsx
@@ -37,7 +37,7 @@ const TailwindTable: FC<TailwindTableProps> = ({ specObj }) => {
     <Table>
       <THead>
         <TR>
-          <TH>Twin Macro</TH>
+          <TH>Macro</TH>
           <TH>Property</TH>
         </TR>
       </THead>
