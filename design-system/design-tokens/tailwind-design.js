0921a2a897796d37f9f43f8274e407faea8727f1
user <progexplorer@gmail.com>
2020-06-10 13:43:10 +0200

    refactor: simplify design-system
    - Remove font weight
    - change font-size
    - remove font folder

--- design-system/design-tokens/tailwind-design.js
+++ design-system/design-tokens/tailwind-design.js
@@ -1,14 +1,13 @@
 const colors = require("./colors");
 const screens = require("./screens");
 const borderRadius = require("./border-radius");
-const fontSize = require("./roboto-type-scale/font-size");
+const fontSize = require("./font-size");
 const spacing = require("./spacing");
 const boxShadow = require("./box-shadow");
 const fontFamily = require("./font-family");
-const letterSpacing = require("./roboto-type-scale/letter-spacing");
+const letterSpacing = require("./letter-spacing");
 const lineHeight = require("./line-height");
 const opacity = require("./opacity");
-const fontWeight = require("./roboto-type-scale/font-weight");
 
 module.exports = {
   colors,
@@ -21,5 +20,4 @@ module.exports = {
   letterSpacing,
   lineHeight,
   opacity,
-  fontWeight,
 };
