f86864b32ef1b0e52a8ac0b5052aa76f551916d6
user <progexplorer@gmail.com>
2020-06-10 13:36:44 +0200

    feat: update font-size data

--- data.ts
+++ data.ts
@@ -1,6 +1,77 @@
 import tw from "twin.macro";
 
-export const defaultFontClasses = {
+export const customFontSizes = {
+  overline: {
+    value: `font-size: 0.625rem;`,
+    twClass: tw`text-overline`,
+  },
+  "text-4": {
+    value: `font-size: 1rem;`,
+    twClass: tw`text-4`,
+  },
+  "text-5": {
+    value: `font-size: 1.25rem;`,
+    twClass: tw`text-5`,
+  },
+  "text-6": {
+    value: `font-size: 1.5rem;`,
+    twClass: tw`text-6`,
+  },
+  "text-7": {
+    value: `font-size: 1.75rem;`,
+    twClass: tw`text-7`,
+  },
+  "text-8": {
+    value: `font-size: 2rem;`,
+    twClass: tw`text-8`,
+  },
+  "text-9": {
+    value: `font-size: 2.25rem;`,
+    twClass: tw`text-9`,
+  },
+  "text-10": {
+    value: `font-size: 2.5rem;`,
+    twClass: tw`text-10`,
+  },
+  "text-11": {
+    value: `font-size: 2.75rem;`,
+    twClass: tw`text-11`,
+  },
+  "text-12": {
+    value: `font-size: 3rem;`,
+    twClass: tw`text-12`,
+  },
+  "text-14": {
+    value: `font-size: 3.5rem;`,
+    twClass: tw`text-14`,
+  },
+  "text-16": {
+    value: `font-size: 4rem;`,
+    twClass: tw`text-16`,
+  },
+  "text-18": {
+    value: `font-size: 4.5rem;`,
+    twClass: tw`text-18`,
+  },
+  "text-20": {
+    value: `font-size: 5rem;`,
+    twClass: tw`text-20`,
+  },
+  "text-22": {
+    value: `font-size: 5.5rem;`,
+    twClass: tw`text-22`,
+  },
+  "text-24": {
+    value: `font-size: 6rem;`,
+    twClass: tw`text-24`,
+  },
+  "text-26": {
+    value: `font-size: 6.5rem;`,
+    twClass: tw`text-26`,
+  },
+};
+
+export const defaultFontSizes = {
   "text-xs": {
     value: `font-size: .75rem;`,
     twClass: tw`text-xs`,
@@ -44,3 +115,42 @@ export const defaultFontClasses = {
 };
 
 export const customFontClasses = {};
+
+export const fontWeight = {
+  "font-hairline": {
+    value: "font-weight:100;",
+    twClass: tw`font-hairline`,
+  },
+  "font-thin": {
+    value: "font-weight:200;",
+    twClass: tw`font-thin`,
+  },
+  "font-light": {
+    value: "font-weight:300;",
+    twClass: tw`font-light`,
+  },
+  "font-normal": {
+    value: "font-weight:400;",
+    twClass: tw`font-normal`,
+  },
+  "font-medium": {
+    value: "font-weight:500;",
+    twClass: tw`font-medium`,
+  },
+  "font-semibold": {
+    value: "font-weight:600;",
+    twClass: tw`font-semibold`,
+  },
+  "font-bold": {
+    value: "font-weight:700;",
+    twClass: tw`font-bold`,
+  },
+  "font-extrabold": {
+    value: "font-weight:800",
+    twClass: tw`font-extrabold`,
+  },
+  "font-black": {
+    value: "font-weight:900",
+    twClass: tw`font-black`,
+  },
+};
