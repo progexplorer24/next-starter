ce8723106c54e6f3931fc544f51c00b9fac1e8fe
user <progexplorer@gmail.com>
2020-06-13 12:32:22 +0200

    refactor: rename component

---
+++ components/docs/render-examples/render-examples.tsx
@@ -0,0 +1,55 @@
+import React, { FC } from "react";
+import { SerializedStyles } from "@emotion/core";
+import { TwStyle } from "twin.macro";
+import { v4 as uuid } from "uuid";
+import { toPairs } from "ramda";
+import Board from "@components/docs/board/board";
+import Example from "../example/example";
+
+type RenderExamplesProps = {
+  css?: SerializedStyles;
+  data: {
+    [key: string]: {
+      value: string;
+      twClass: TwStyle;
+    };
+  };
+};
+
+const RenderExamples: FC<RenderExamplesProps> = ({ data = {}, ...props }) => {
+  const pairKeysAndValues = toPairs(data);
+
+  const renderExamples = (arr: typeof pairKeysAndValues): JSX.Element => (
+    <>
+      {arr.map(([key, { twClass }]) => {
+        return <Example key={uuid()} twClass={twClass} classKey={key} />;
+      })}
+    </>
+  );
+  return <Board {...props}>{renderExamples(pairKeysAndValues)}</Board>;
+};
+
+export default RenderExamples;
+
+// return (
+//   <div
+//     css={css`
+//       ${tw`px-2 mt-4`}
+//     `}
+//     key={uuid()}
+//   >
+//     <span
+//       css={css`
+//         ${tw`text-sm font-bold text-gray-600 `}
+//       `}
+//     >
+//       .{key}
+//     </span>
+//     <Text
+//       css={css`
+//         ${twClass}
+//         ${shouldTruncate(fontSize) ? tw`truncate` : undefined}
+//       `}
+//     />
+//   </div>
+// );
