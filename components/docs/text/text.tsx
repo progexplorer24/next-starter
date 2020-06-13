278a76faaf597f99b1a8234cf90dd87e35b40999
user <progexplorer@gmail.com>
2020-06-13 12:36:11 +0200

    feat: truncate component text

--- components/docs/text/text.tsx
+++ components/docs/text/text.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react";
-import { SerializedStyles } from "@emotion/core";
+import { SerializedStyles, css } from "@emotion/core";
+import tw from "twin.macro";
 
-type TextProps = {
+export type TextProps = {
   css?: SerializedStyles;
   text?: string;
 };
@@ -9,7 +10,16 @@ type TextProps = {
 const defaultText = `The quick brown fox jumped over the lazy dog.`;
 
 const Text: FC<TextProps> = ({ text = defaultText, ...props }) => {
-  return <p {...props}>{text}</p>;
+  return (
+    <p
+      css={css`
+        ${tw`truncate`}
+      `}
+      {...props}
+    >
+      {text}
+    </p>
+  );
 };
 
 export default Text;
