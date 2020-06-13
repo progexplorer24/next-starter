b2baae71266c79addd91f68b19d549b286736e22
user <progexplorer@gmail.com>
2020-06-13 22:51:21 +0200

    test: simplify cypress webpack config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,52 +19,56 @@
 const webpackPreprocessor = require("@cypress/webpack-preprocessor");
 const path = require("path");
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const webpackConfig = {
+  mode: "development",
+  resolve: {
+    extensions: [".ts", ".js", ".jsx", ".tsx", ".json"],
+    alias: {
+      "@components": path.resolve(__dirname, "../../components"),
+      "@styles": path.resolve(__dirname, "../../styles"),
+    },
+  },
+  node: {
+    // Import export errors solution: https://github.com/azu/rc-config-loader/issues/2
+    fs: "empty",
+    module: "empty",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$|\.tsx?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "next/babel",
+                "@babel/preset-env",
+                "@emotion/babel-preset-css-prop",
+                "@babel/preset-typescript",
+              ],
+              plugins: ["macros", "transform-class-properties", "istanbul"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+    ],
+  },
+};
+
 module.exports = (on, config) => {
   // eslint-disable-next-line global-require
-  require("cypress-react-unit-test/plugins/react-scripts")(on, config);
+  require("cypress-react-unit-test/plugins/babelrc")(on, config);
   // eslint-disable-next-line global-require
   require("@cypress/code-coverage/task")(on, config);
 
-  const webpack = {
-    mode: "development",
-    resolve: {
-      extensions: [".ts", ".js", ".jsx", ".tsx", ".json"],
-      alias: {
-        "@components": path.resolve(__dirname, "../../components"),
-      },
-    },
-    node: {
-      // Import export errors solution: https://github.com/azu/rc-config-loader/issues/2
-      fs: "empty",
-      module: "empty",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$|\.tsx?$/,
-          exclude: [/node_modules/],
-          use: [
-            {
-              loader: "babel-loader",
-              options: {
-                presets: [
-                  "next/babel",
-                  "@babel/preset-env",
-                  "@emotion/babel-preset-css-prop",
-                  "@babel/preset-typescript",
-                ],
-                plugins: ["macros", "transform-class-properties", "istanbul"],
-              },
-            },
-          ],
-        },
-      ],
-    },
-  };
-
   const options = {
-    webpackOptions: webpack,
+    webpackOptions: webpackConfig,
   };
 
   on("file:preprocessor", webpackPreprocessor(options));
