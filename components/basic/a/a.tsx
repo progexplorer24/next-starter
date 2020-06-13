d8d1aaef1786e26d75be9e4a6cfb1483c515e1d3
user <progexplorer@gmail.com>
2020-06-13 12:26:12 +0200

    fix: pass href to a tag

--- components/basic/a/a.tsx
+++ components/basic/a/a.tsx
@@ -3,11 +3,12 @@ import { css } from "@emotion/core";
 import tw from "twin.macro";
 import AAtom, { AProps } from "@components/atoms/a/a";
 
-const A: FC<AProps> = ({ children, ...props }) => (
+const A: FC<AProps> = ({ children, href, ...props }) => (
   <AAtom
     css={css`
       ${tw`text-blue-700`}
     `}
+    href={href}
     {...props}
   >
     {children}
