e6e7ec727d182ac461cd382b246219bd3922965d
user <progexplorer@gmail.com>
2020-06-13 13:08:05 +0200

    feat: pass styles to copy button

--- components/code/copy-button/copy-button.tsx
+++ components/code/copy-button/copy-button.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { css } from "@emotion/core";
+import { css, SerializedStyles } from "@emotion/core";
 import tw from "twin.macro";
 
 type CopyButtonProps = {
+  css?: SerializedStyles;
   handleClick: () => void;
 };
 
-const CopyButton: React.FC<CopyButtonProps> = ({ handleClick }) => {
-  const positionButton = tw`absolute right-0 z-10 px-4 h-11`;
+const CopyButton: React.FC<CopyButtonProps> = ({ handleClick, ...props }) => {
+  const positionButton = tw`absolute right-0 z-10 px-4 py-2`;
 
   return (
     <button
@@ -17,6 +18,7 @@ const CopyButton: React.FC<CopyButtonProps> = ({ handleClick }) => {
         ${positionButton}
         ${tw`text-sm font-bold tracking-widest text-gray-100 bg-blue-700 rounded-tr-lg rounded-bl-lg hocus:bg-blue-600 hocus:text-white focus:outline-none`}
       `}
+      {...props}
     >
       Copy
     </button>
