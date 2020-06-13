07adf9f67fbd579d13c8dba14c14ceb1f26a26b0
user <progexplorer@gmail.com>
2020-06-13 13:13:44 +0200

    feat: update data with spacing values

--- data.ts
+++ data.ts
@@ -114,7 +114,220 @@ export const defaultFontSizes = {
   },
 };
 
-export const customFontClasses = {};
+export const spacing = {
+  "0": {
+    value: "0rem / 0px",
+    twClass: tw`w-0`,
+  },
+  px: {
+    value: "0.0625rem / 1px",
+    twClass: tw`w-px`,
+  },
+  "1": {
+    value: "0.25rem / 4px",
+    twClass: tw`w-1`,
+  },
+  "2": {
+    value: "0.5rem / 8px",
+    twClass: tw`w-2`,
+  },
+  "3": {
+    value: "0.75rem / 12px",
+    twClass: tw`w-3`,
+  },
+  "4": {
+    value: "1rem / 16px",
+    twClass: tw`w-4`,
+  },
+  "5": {
+    value: "1.25rem / 20px",
+    twClass: tw`w-5`,
+  },
+  "6": {
+    value: "1.5rem / 24px",
+    twClass: tw`w-6`,
+  },
+  "7": {
+    value: "1.75rem / 28px",
+    twClass: tw`w-7`,
+  },
+  "8": {
+    value: "2rem / 32px",
+    twClass: tw`w-8`,
+  },
+  "9": {
+    value: "2.25rem / 36px",
+    twClass: tw`w-9`,
+  },
+  "10": {
+    value: "2.5rem / 40px",
+    twClass: tw`w-10`,
+  },
+  "11": {
+    value: "2.75rem / 44px",
+    twClass: tw`w-11`,
+  },
+  "12": {
+    value: "3rem / 48px",
+    twClass: tw`w-12`,
+  },
+  "14": {
+    value: "3.5rem / 56px",
+    twClass: tw`w-14`,
+  },
+  "16": {
+    value: "4rem / 64px",
+    twClass: tw`w-16`,
+  },
+  "18": {
+    value: "4.5rem / 72px",
+    twClass: tw`w-18`,
+  },
+  "20": {
+    value: "5rem / 80px",
+    twClass: tw`w-20`,
+  },
+  "22": {
+    value: "5.5rem / 88px",
+    twClass: tw`w-22`,
+  },
+  "24": {
+    value: "6rem / 96px",
+    twClass: tw`w-24`,
+  },
+  "26": {
+    value: "6.5rem / 104px",
+    twClass: tw`w-26`,
+  },
+  "28": {
+    value: "7rem / 112px",
+    twClass: tw`w-28`,
+  },
+  "30": {
+    value: "7.5rem / 120px",
+    twClass: tw`w-30`,
+  },
+  "32": {
+    value: "8rem / 128px",
+    twClass: tw`w-32`,
+  },
+  "34": {
+    value: "8.5rem / 136px",
+    twClass: tw`w-34`,
+  },
+  "36": {
+    value: "9rem / 144px",
+    twClass: tw`w-36`,
+  },
+  "38": {
+    value: "9.5rem / 152px",
+    twClass: tw`w-38`,
+  },
+  "40": {
+    value: "10rem / 160px",
+    twClass: tw`w-40`,
+  },
+  "42": {
+    value: "10.5rem / 168px",
+    twClass: tw`w-42`,
+  },
+  "44": {
+    value: "11rem / 176px",
+    twClass: tw`w-44`,
+  },
+  "46": {
+    value: "11.5rem / 184px",
+    twClass: tw`w-46`,
+  },
+  "48": {
+    value: "12rem / 192px",
+    twClass: tw`w-48`,
+  },
+  "50": {
+    value: "12.5rem / 200px",
+    twClass: tw`w-50`,
+  },
+  "52": {
+    value: "13rem / 208px",
+    twClass: tw`w-52`,
+  },
+  "54": {
+    value: "13.5rem / 216px",
+    twClass: tw`w-54`,
+  },
+  "56": {
+    value: "14rem / 224px",
+    twClass: tw`w-56`,
+  },
+  "58": {
+    value: "14.5rem / 232px",
+    twClass: tw`w-58`,
+  },
+  "60": {
+    value: "15rem / 240px",
+    twClass: tw`w-60`,
+  },
+  "62": {
+    value: "15.5rem / 248px",
+    twClass: tw`w-62`,
+  },
+  "64": {
+    value: "16rem / 256px",
+    twClass: tw`w-64`,
+  },
+  "66": {
+    value: "16.5rem / 264px",
+    twClass: tw`w-66`,
+  },
+  "68": {
+    value: "17rem / 272px",
+    twClass: tw`w-68`,
+  },
+  "70": {
+    value: "17.5rem / 280px",
+    twClass: tw`w-70`,
+  },
+  "72": {
+    value: "18rem / 288px",
+    twClass: tw`w-72`,
+  },
+  "74": {
+    value: "18.5rem / 296px",
+    twClass: tw`w-74`,
+  },
+  "76": {
+    value: "19rem / 304px",
+    twClass: tw`w-76`,
+  },
+  "78": {
+    value: "19.5rem / 312px",
+    twClass: tw`w-78`,
+  },
+  "80": {
+    value: "20rem / 320px",
+    twClass: tw`w-80`,
+  },
+  "84": {
+    value: "21rem / 336px",
+    twClass: tw`w-84`,
+  },
+  "88": {
+    value: "22rem / 352px",
+    twClass: tw`w-88`,
+  },
+  "92": {
+    value: "23rem / 368px",
+    twClass: tw`w-92`,
+  },
+  "96": {
+    value: "24rem / 384px",
+    twClass: tw`w-96`,
+  },
+  "100": {
+    value: "25rem / 400px",
+    twClass: tw`w-100`,
+  },
+};
 
 export const fontWeight = {
   "font-hairline": {
