3fce5d639a32f570bbeb12e046a7248ed1cbea74
user <progexplorer@gmail.com>
2020-06-14 14:52:26 +0200

    refactor: extract cloneEmotion to separate file
    Give more descriptive prop names

--- components/atoms/lists/ul/ul.tsx
+++ components/atoms/lists/ul/ul.tsx
@@ -1,21 +1,21 @@
 import React, { FC } from "react";
 import { SerializedStyles, css } from "@emotion/core";
 import tw from "twin.macro";
-import RightArrow from "../li/right-arrow.svg";
+import cloneEmotion from "utils/emotion-clone";
+import RightArrow from "./right-arrow.svg";
 
 export type ULProps = {
   css?: SerializedStyles;
+  icon?: JSX.Element;
   children: JSX.Element[];
 };
 
-const UL: FC<ULProps> = ({ children, ...props }) => {
-  const listType = (
-    <RightArrow
-      css={css`
-        ${tw`inline-flex w-5 h-5 mr-2 text-gray-600 fill-current`}
-      `}
-    />
-  );
+const UL: FC<ULProps> = ({ children, icon = <RightArrow />, ...props }) => {
+  const iconStyles = css`
+    ${tw`inline-flex w-5 h-5 mr-2 text-gray-600 fill-current`}
+  `;
+
+  const iconWithStyles = cloneEmotion(icon, { css: iconStyles });
 
   return (
     <ul
@@ -25,7 +25,7 @@ const UL: FC<ULProps> = ({ children, ...props }) => {
       {...props}
     >
       {React.Children.map(children, (child) =>
-        React.cloneElement(child, { listType })
+        React.cloneElement(child, { listDecoration: iconWithStyles })
       )}
     </ul>
   );
