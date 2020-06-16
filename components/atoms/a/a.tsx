b43da8c22bc0eafd830a615590c66277c8ef168e
user <progexplorer@gmail.com>
2020-06-16 10:40:25 +0200

    feat: refactor css prop & add className prop

--- components/atoms/a/a.tsx
+++ components/atoms/a/a.tsx
@@ -4,17 +4,18 @@ import tw from "twin.macro";
 
 export type AProps = {
   css?: SerializedStyles;
+  className?: string;
   href: string;
   children: ReactNode;
 };
 
 const A = React.forwardRef<HTMLAnchorElement, AProps>(
-  ({ children, href, ...props }, ref) => {
+  ({ children, href, css, ...props }, ref) => {
     return (
       <a
         ref={ref}
         href={href}
-        css={emotionCss([tw`font-bold underline cursor-pointer`])}
+        css={emotionCss([tw`font-bold underline cursor-pointer`, css])}
         {...props}
       >
         {children}
