1a09176dba3ce036a5b11e7b8702899caa294753
user <progexplorer@gmail.com>
2020-06-17 11:03:22 +0200

    feat: add custom shadow object

--- data.ts
+++ data.ts
@@ -414,3 +414,136 @@ export const defaultShadows = {
     twClass: tw`shadow-none`,
   },
 };
+
+export const customShadows = {
+  "shadow-1": {
+    value:
+      "0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12)",
+    twClass: tw`shadow-1`,
+  },
+  "shadow-2": {
+    value:
+      "0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12)",
+    twClass: tw`shadow-2`,
+  },
+  "shadow-3": {
+    value:
+      "0 3px 3px -2px rgba(0,0,0,.2), 0 3px 4px 0 rgba(0,0,0,.14), 0 1px 8px 0 rgba(0,0,0,.12)",
+    twClass: tw`shadow-3`,
+  },
+
+  "shadow-4": {
+    value:
+      "0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12)",
+    twClass: tw`shadow-4`,
+  },
+
+  "shadow-5": {
+    value:
+      "0 3px 5px -1px rgba(0,0,0,.2), 0 5px 8px 0 rgba(0,0,0,.14), 0 1px 14px 0 rgba(0,0,0,.12)",
+    twClass: tw`shadow-5`,
+  },
+  "shadow-6": {
+    value:
+      "0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12)",
+    twClass: tw`shadow-6`,
+  },
+
+  "shadow-7": {
+    value:
+      "0 4px 5px -2px rgba(0,0,0,.2), 0 7px 10px 1px rgba(0,0,0,.14), 0 2px 16px 1px rgba(0,0,0,.12)",
+    twClass: tw`shadow-7`,
+  },
+  "shadow-8": {
+    value:
+      "0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12)",
+    twClass: tw`shadow-8`,
+  },
+  "shadow-9": {
+    value:
+      "0 5px 6px -3px rgba(0,0,0,.2), 0 9px 12px 1px rgba(0,0,0,.14), 0 3px 16px 2px rgba(0,0,0,.12)",
+    twClass: tw`shadow-9`,
+  },
+  "shadow-10": {
+    value:
+      "0 6px 6px -3px rgba(0,0,0,.2), 0 10px 14px 1px rgba(0,0,0,.14), 0 4px 18px 3px rgba(0,0,0,.12)",
+    twClass: tw`shadow-10`,
+  },
+  "shadow-11": {
+    value:
+      "0 6px 7px -4px rgba(0,0,0,.2), 0 11px 15px 1px rgba(0,0,0,.14), 0 4px 20px 3px rgba(0,0,0,.12)",
+    twClass: tw`shadow-11`,
+  },
+  "shadow-12": {
+    value:
+      "0 7px 8px -4px rgba(0,0,0,.2), 0 12px 17px 2px rgba(0,0,0,.14), 0 5px 22px 4px rgba(0,0,0,.12)",
+    twClass: tw`shadow-12`,
+  },
+  "shadow-13": {
+    value:
+      "0 7px 8px -4px rgba(0,0,0,.2), 0 13px 19px 2px rgba(0,0,0,.14), 0 5px 24px 4px rgba(0,0,0,.12)",
+    twClass: tw`shadow-13`,
+  },
+  "shadow-14": {
+    value:
+      "0 7px 9px -4px rgba(0,0,0,.2), 0 14px 21px 2px rgba(0,0,0,.14), 0 5px 26px 4px rgba(0,0,0,.12)",
+    twClass: tw`shadow-14`,
+  },
+  "shadow-15": {
+    value:
+      "0 8px 9px -5px rgba(0,0,0,.2), 0 15px 22px 2px rgba(0,0,0,.14), 0 6px 28px 5px rgba(0,0,0,.12)",
+    twClass: tw`shadow-15`,
+  },
+  "shadow-16": {
+    value:
+      "0 8px 10px -5px rgba(0,0,0,.2), 0 16px 24px 2px rgba(0,0,0,.14), 0 6px 30px 5px rgba(0,0,0,.12)",
+    twClass: tw`shadow-16`,
+  },
+  "shadow-17": {
+    value:
+      "0 8px 11px -5px rgba(0,0,0,.2), 0 17px 26px 2px rgba(0,0,0,.14), 0 6px 32px 5px rgba(0,0,0,.12)",
+    twClass: tw`shadow-17`,
+  },
+
+  "shadow-18": {
+    value:
+      "0 9px 11px -5px rgba(0,0,0,.2), 0 18px 28px 2px rgba(0,0,0,.14), 0 7px 34px 6px rgba(0,0,0,.12)",
+    twClass: tw`shadow-18`,
+  },
+
+  "shadow-19": {
+    value:
+      "0 9px 12px -6px rgba(0,0,0,.2), 0 19px 29px 2px rgba(0,0,0,.14), 0 7px 36px 6px rgba(0,0,0,.12)",
+    twClass: tw`shadow-19`,
+  },
+
+  "shadow-20": {
+    value:
+      "0 10px 13px -6px rgba(0,0,0,.2), 0 20px 31px 3px rgba(0,0,0,.14), 0 8px 38px 7px rgba(0,0,0,.12)",
+    twClass: tw`shadow-20`,
+  },
+
+  "shadow-21": {
+    value:
+      "0 10px 13px -6px rgba(0,0,0,.2), 0 21px 33px 3px rgba(0,0,0,.14), 0 8px 40px 7px rgba(0,0,0,.12)",
+    twClass: tw`shadow-21`,
+  },
+
+  "shadow-22": {
+    value:
+      "0 10px 14px -6px rgba(0,0,0,.2), 0 22px 35px 3px rgba(0,0,0,.14), 0 8px 42px 7px rgba(0,0,0,.12)",
+    twClass: tw`shadow-22`,
+  },
+
+  "shadow-23": {
+    value:
+      "0 11px 14px -7px rgba(0,0,0,.2), 0 23px 36px 3px rgba(0,0,0,.14), 0 9px 44px 8px rgba(0,0,0,.12)",
+    twClass: tw`shadow-23`,
+  },
+
+  "shadow-24": {
+    value:
+      "0 11px 15px -7px rgba(0,0,0,.2), 0 24px 38px 3px rgba(0,0,0,.14), 0 9px 46px 8px rgba(0,0,0,.12)",
+    twClass: tw`shadow-24`,
+  },
+};
