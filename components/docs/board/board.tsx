1e7b45e2da4424b2b5300a2b738403ed51456044
user <progexplorer@gmail.com>
2020-06-17 11:20:36 +0200

    fix: emotion css prop styling composition

--- components/docs/board/board.tsx
+++ components/docs/board/board.tsx
@@ -1,18 +1,20 @@
 import React, { ReactNode, ReactElement } from "react";
-import { SerializedStyles, css } from "@emotion/core";
+import { SerializedStyles, css as emotionCss } from "@emotion/core";
 import tw from "twin.macro";
 
 type BoardProps = {
   css?: SerializedStyles;
+  className?: string;
   children: ReactNode;
 };
 
-const Board = ({ children, ...props }: BoardProps): ReactElement => {
+const Board = ({ children, css, ...props }: BoardProps): ReactElement => {
   return (
     <div
-      css={css`
-        ${tw`p-2 mt-6 bg-gray-100 border border-gray-400 rounded-lg shadow-lg`}
-      `}
+      css={emotionCss([
+        tw`p-4 mt-6 bg-gray-100 border border-gray-400 rounded-lg shadow-lg`,
+        css,
+      ])}
       {...props}
     >
       {children}
