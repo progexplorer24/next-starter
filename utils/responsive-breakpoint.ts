a392e4add47b0aabeea4506afb41e96d68ec1578
user <progexplorer@gmail.com>
2020-06-19 12:27:39 +0200

    feat: add responsive breakpoint utility

---
+++ utils/responsive-breakpoint.ts
@@ -0,0 +1,30 @@
+import { css, SerializedStyles } from "@emotion/core";
+import { TwStyle } from "twin.macro";
+/**
+ *
+ * @param breakpoint number in pixels where set min-width of a break point
+ * @param styles pass here emotion css prop or tailwind macro styles
+ *
+ * Example: breakpoint(300)(tw`bg-purple-400`) - It will add a purple background to the element at 300px breakpoint and higher.
+ */
+
+const responsiveBreakpoint = (breakpoint: number) => (
+  styles: SerializedStyles | TwStyle
+): SerializedStyles => css`
+  @media (min-width: ${breakpoint}px) {
+    ${styles}
+  }
+`;
+
+// const responsiveBreakpoint = (
+//   breakpoint: number,
+//   styles: SerializedStyles
+// ): SerializedStyles => {
+//   return css`
+//     @media (min-width: ${breakpoint}px) {
+//       ${styles}
+//     }
+//   `;
+// };
+
+export default responsiveBreakpoint;
