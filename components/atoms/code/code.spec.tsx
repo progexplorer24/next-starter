50b64f1101587197197c91bf4ff353299d58ab6b
user <progexplorer@gmail.com>
2020-06-19 12:10:04 +0200

    test: add code spec file

---
+++ components/atoms/code/code.spec.tsx
@@ -0,0 +1,8 @@
+// import React from "react";
+// import { mount } from "cypress-react-unit-test";
+// import { css } from "@emotion/core";
+// import tw from "twin.macro";
+
+before(() => {
+  cy.addGlobalStyles();
+});
