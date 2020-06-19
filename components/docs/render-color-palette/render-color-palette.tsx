e4987093a5ed2a35710a0a69f244df1196b61e57
user <progexplorer@gmail.com>
2020-06-19 10:55:01 +0200

    refactor: remove centering of elements

--- components/docs/render-color-palette/render-color-palette.tsx
+++ components/docs/render-color-palette/render-color-palette.tsx
@@ -35,10 +35,8 @@ const RenderColorPalette = ({
     { value: string; twClass: TwStyle }
   ]) => {
     return (
-      <div css={emotionCss([tw`sm:flex sm:items-center sm:justify-center`])}>
-        <ColorCircle
-          css={emotionCss([tw`mx-auto bg-red-100 sm:mx-0`, twClass])}
-        />
+      <div key={uuid()} css={emotionCss([tw`sm:flex sm:items-center `])}>
+        <ColorCircle css={emotionCss([tw`bg-red-100 sm:mx-0`, twClass])} />
         <div
           css={emotionCss`
             ${tw`mt-2 sm:pl-3 sm:mt-2`}
@@ -46,16 +44,12 @@ const RenderColorPalette = ({
         >
           <div
             css={emotionCss([
-              tw`text-xs font-bold tracking-wider text-center text-gray-700`,
+              tw`text-xs font-bold tracking-wider text-gray-700`,
             ])}
           >
             {shade}
           </div>
-          <div
-            css={emotionCss([
-              tw`text-sm tracking-wider text-center text-gray-700`,
-            ])}
-          >
+          <div css={emotionCss([tw`text-sm tracking-wider text-gray-700`])}>
             {value}
           </div>
         </div>
@@ -64,21 +58,21 @@ const RenderColorPalette = ({
   };
 
   return (
-    <div
-      {...props}
-      css={emotionCss([tw`grid grid-cols-2 md:grid-cols-3`, css])}
-    >
+    <div {...props} css={emotionCss([tw`grid grid-cols-2`, css])}>
       {headingObjectPairs.map(([color, obj]) => (
         <div key={uuid()}>
-          <H4 css={emotionCss([tw`text-center capitalize`])}>{color}</H4>
-          {isBlackOrWhite(color) ? undefined : (
-            <div
-              css={emotionCss([
-                tw`h-40 mx-auto rounded-full w-14 sm:w-16`,
-                obj[400].twClass,
-              ])}
-            />
-          )}
+          <div>
+            <H4 css={emotionCss([tw`capitalize`])}>{color}</H4>
+            {isBlackOrWhite(color) ? undefined : (
+              <div
+                css={emotionCss([
+                  tw`h-40 mt-4 rounded-full w-14 sm:w-40 sm:h-16`,
+                  obj[400].twClass,
+                ])}
+              />
+            )}
+          </div>
+
           {map(renderShades, toPairs(obj))}
         </div>
       ))}
