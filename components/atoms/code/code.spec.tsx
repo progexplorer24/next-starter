b07c9a5573f25f6d6809942ed1a607c5c1389ce3
user <progexplorer@gmail.com>
2020-06-20 11:54:37 +0200

    test: create code tests

--- components/atoms/code/code.spec.tsx
+++ components/atoms/code/code.spec.tsx
@@ -1,8 +1,32 @@
-// import React from "react";
-// import { mount } from "cypress-react-unit-test";
-// import { css } from "@emotion/core";
-// import tw from "twin.macro";
+import React from "react";
+import { mount } from "cypress-react-unit-test";
+import { css as emotionCss } from "@emotion/core";
+import tw from "twin.macro";
+import Code from "./code";
 
 before(() => {
   cy.addGlobalStyles();
+});
+
+const text = "background-color: red;";
+
+const Component = () => <div data-cy="component">Hello</div>;
+
+describe("Code Component", () => {
+  it("renders text", () => {
+    mount(<Code>{text}</Code>);
+    cy.contains(text);
+  });
+  it("renders another component", () => {
+    mount(
+      <Code>
+        <Component />
+      </Code>
+    );
+    cy.get("[data-cy=component]").should("exist");
+  });
+  it("works with emotion and tailwindcss styling", () => {
+    mount(<Code css={emotionCss([tw`flex`])}>{text}</Code>);
+    cy.get("code").should("have.css", "display", "flex");
+  });
 });
