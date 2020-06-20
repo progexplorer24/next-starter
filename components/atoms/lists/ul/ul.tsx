12237c37cede3e1af5dc5a53db88c75a8e800342
user <progexplorer@gmail.com>
2020-06-20 14:05:29 +0200

    refactor: children type and icon indent

--- components/atoms/lists/ul/ul.tsx
+++ components/atoms/lists/ul/ul.tsx
@@ -3,12 +3,15 @@ import { SerializedStyles, css as emotionCss } from "@emotion/core";
 import tw from "twin.macro";
 import cloneEmotion from "utils/emotion-clone";
 import RightArrow from "./right-arrow.svg";
+import type { LiProps } from "../li/li";
 
 export type UlProps = {
   css?: SerializedStyles;
   className?: string;
-  icon?: JSX.Element;
-  children: JSX.Element[];
+  icon?: ReactElement;
+  children:
+    | ReactElement<LiProps, (props: LiProps) => ReactElement>
+    | ReactElement<LiProps, (props: LiProps) => ReactElement>[];
 };
 
 const Ul = ({
@@ -18,7 +21,7 @@ const Ul = ({
   ...props
 }: UlProps): ReactElement => {
   const iconStyles = emotionCss`
-    ${tw`inline-flex w-5 h-5 mr-2 text-gray-600 fill-current`}
+    ${tw`absolute top-0 left-0 w-5 h-5 mt-1 text-gray-600 fill-current`}
   `;
 
   const iconWithStyles = cloneEmotion(icon, { css: iconStyles });
@@ -26,7 +29,10 @@ const Ul = ({
   return (
     <ul css={emotionCss([tw`mt-6`, css])} {...props}>
       {React.Children.map(children, (child) =>
-        React.cloneElement(child, { listDecoration: iconWithStyles })
+        React.cloneElement(child, {
+          listDecoration: iconWithStyles,
+          css: emotionCss([tw`relative pl-7`]),
+        })
       )}
     </ul>
   );
