5f9ffb7e7ec355a218d995d1199d15c1a2522573
user <progexplorer@gmail.com>
2020-06-21 17:30:27 +0200

    refactor: change palette heading

--- components/docs/render-color-palette/render-color-palette.tsx
+++ components/docs/render-color-palette/render-color-palette.tsx
@@ -3,9 +3,10 @@ import { SerializedStyles, css as emotionCss } from "@emotion/core";
 import { v4 as uuid } from "uuid";
 import { map, toPairs } from "ramda";
 import tw, { TwStyle } from "twin.macro";
-import H4 from "@components/basic/h4/h4";
+
 import { colors } from "data";
 import responsiveBreakpoint from "utils/responsive-breakpoint";
+import H2 from "@components/basic/h2/h2";
 import ColorCircle from "../color-circle/color-circle";
 
 type RenderColorPaletteProps = {
@@ -64,7 +65,7 @@ const RenderColorPalette = ({
       {headingObjectPairs.map(([color, obj]) => (
         <div key={uuid()}>
           <div>
-            <H4 css={emotionCss([tw`capitalize`])}>{color}</H4>
+            <H2 css={emotionCss([tw`capitalize`])}>{color}</H2>
             {isBlackOrWhite(color) ? undefined : (
               <div
                 css={emotionCss([
