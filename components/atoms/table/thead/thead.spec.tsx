918d919c61f93aee145099864917d48841f38d26
user <progexplorer@gmail.com>
2020-06-21 17:24:22 +0200

    test: add thead spec

---
+++ components/atoms/table/thead/thead.spec.tsx
@@ -0,0 +1,63 @@
+import React, { ReactElement } from "react";
+import { mount } from "cypress-react-unit-test";
+import { css as emotionCss } from "@emotion/core";
+import tw from "twin.macro";
+import Table from "../table";
+import Thead from "./thead";
+import type { TrProps } from "../tr/tr";
+
+before(() => {
+  cy.addGlobalStyles();
+});
+
+const Component = (): ReactElement<
+  TrProps,
+  (props: TrProps) => ReactElement
+> => <tr data-cy="component">Hello</tr>;
+
+describe("Table Head Component", () => {
+  it("renders with no tr components", () => {
+    mount(
+      <Table>
+        <Thead />
+      </Table>
+    );
+    cy.get("thead").should("exist");
+  });
+  it("renders single tr component", () => {
+    mount(
+      <Table>
+        <Thead>
+          <Component />
+        </Thead>
+      </Table>
+    );
+    cy.get("[data-cy=component]")
+      .should("exist")
+      .and("be.visible")
+      .and("have.length", 1);
+  });
+  it("renders multiple tr components", () => {
+    mount(
+      <Table>
+        <Thead>
+          <Component />
+          <Component />
+          <Component />
+        </Thead>
+      </Table>
+    );
+    cy.get("[data-cy=component]")
+      .should("exist")
+      .and("be.visible")
+      .and("have.length", 3);
+  });
+  it("works with emotion and tailwindcss styling", () => {
+    mount(
+      <Table>
+        <Thead css={emotionCss([tw`flex`])} />
+      </Table>
+    );
+    cy.get("thead").should("have.css", "display", "flex");
+  });
+});
