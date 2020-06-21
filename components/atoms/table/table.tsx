dffe93ac9edebe3d5ef373b459a94b2326b44462
user <progexplorer@gmail.com>
2020-06-21 17:26:44 +0200

    refactor: refactor children types

--- components/atoms/table/table.tsx
+++ components/atoms/table/table.tsx
@@ -1,11 +1,22 @@
-import { ReactNode, ReactElement } from "react";
+import { ReactElement } from "react";
 import { css as emotionCss, SerializedStyles } from "@emotion/core";
 import tw from "twin.macro";
+import type { TbodyProps } from "./tbody/tbody";
+import type { TheadProps } from "./thead/thead";
+import type { TrProps } from "./tr/tr";
 
 export type TableProps = {
   css?: SerializedStyles;
   className?: string;
-  children: ReactNode;
+  children?:
+    | ReactElement<
+        TbodyProps | TheadProps | TrProps,
+        (props: TbodyProps | TheadProps | TrProps) => ReactElement
+      >
+    | ReactElement<
+        TbodyProps | TheadProps | TrProps,
+        (props: TbodyProps | TheadProps | TrProps) => ReactElement
+      >[];
 };
 
 const Table = ({ children, css, ...props }: TableProps): ReactElement => (
