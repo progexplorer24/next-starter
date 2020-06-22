748d002cb082e601b3edc9fb09bfb8d3253f0e51
user <progexplorer@gmail.com>
2020-06-22 15:47:20 +0200

    build: add mini-svg-data-uri module

--- declarations.d.ts
+++ declarations.d.ts
@@ -11,3 +11,8 @@ declare module "*.mdx" {
   const MDXComponent: <T1>(props: T1) => JSX.Element;
   export default MDXComponent;
 }
+
+declare module "mini-svg-data-uri" {
+  export const svgToMiniDataUri: (svgString: string) => string;
+  export default svgToMiniDataUri;
+}
