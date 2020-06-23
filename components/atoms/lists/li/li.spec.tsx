3deeec959c483889d130d20c7fb78e73a28f406d
user <progexplorer@gmail.com>
2020-06-23 11:08:45 +0200

    test: update li tests

--- components/atoms/lists/li/li.spec.tsx
+++ components/atoms/lists/li/li.spec.tsx
@@ -3,7 +3,6 @@ import { mount } from "cypress-react-unit-test";
 import { css as emotionCss } from "@emotion/core";
 import tw from "twin.macro";
 import Li from "./li";
-import RightArrow from "../ul/right-arrow.svg";
 
 before(() => {
   cy.addGlobalStyles();
@@ -19,33 +18,16 @@ describe("List item Component", () => {
     cy.contains(text);
   });
 
-  it("renders text with an icon", () => {
-    mount(
-      <Li
-        listDecoration={
-          <RightArrow css={emotionCss([tw`inline-block w-6 h-6 pt-2 mr-2`])} />
-        }
-      >
-        {text}
-      </Li>
-    );
-    cy.contains(text);
-    cy.get("svg").should("exist");
-  });
   it("renders another component", () => {
     mount(
       <Li>
         <Component />
       </Li>
     );
-    cy.get("[data-cy=component]").should("exist");
+    cy.get("[data-cy=component]").should("exist").and("be.visible");
   });
   it("works with emotion and tailwindcss styling", () => {
     mount(<Li css={emotionCss([tw`clear-left`])}>{text}</Li>);
     cy.get("li").should("have.css", "clear", "left");
-  });
-  it("can override default styles", () => {
-    mount(<Li css={emotionCss([tw`block`])}>{text}</Li>);
-    cy.get("li").should("have.css", "display", "block");
   });
 });
