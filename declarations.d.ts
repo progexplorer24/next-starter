9c39023734636a77820cafb3f95678b0e968ce35
user <progexplorer@gmail.com>
2020-06-25 09:38:00 +0200

    build: update type declarations

--- declarations.d.ts
+++ declarations.d.ts
@@ -16,3 +16,9 @@ declare module "mini-svg-data-uri" {
   export const svgToMiniDataUri: (svgString: string) => string;
   export default svgToMiniDataUri;
 }
+
+declare type EmotionProps = {
+  css?: SerializedStyles;
+  className?: string;
+  __EMOTION_TYPE_PLEASE_DO_NOT_USE__?: (props: EmotionProps) => ReactElement;
+};
