f02ad415e5b4c366db519ab5ae805f868c812520
user <progexplorer@gmail.com>
2020-06-25 09:48:30 +0200

    feat: create examples for alert components

---
+++ components/alert/examples.tsx
@@ -0,0 +1,31 @@
+import Alert from "./alert";
+import AlertInfo from "./variations/alert-info";
+import AlertWarning from "./variations/alert-warning";
+import AlertError from "./variations/alert-error";
+import AlertSuccess from "./variations/alert-success";
+
+export const Basic = (): JSX.Element => {
+  return (
+    <div>
+      <Alert>This alert is a basic building block.</Alert>
+      <AlertInfo>Here&lsquo;s some more information.</AlertInfo>
+      <AlertWarning>Important. Your actions have consequences</AlertWarning>
+      <AlertError>Look out! Something went wrong. </AlertError>
+      <AlertSuccess>Success! You did it.</AlertSuccess>
+    </div>
+  );
+};
+
+export const Outlined = (): JSX.Element => {
+  return (
+    <div>
+      <Alert outlined>This alert is a basic building block.</Alert>
+      <AlertInfo outlined>Here&lsquo;s some more information.</AlertInfo>
+      <AlertWarning outlined>
+        Important. Your actions have consequences
+      </AlertWarning>
+      <AlertError outlined>Look out! Something went wrong. </AlertError>
+      <AlertSuccess outlined>Success! You did it.</AlertSuccess>
+    </div>
+  );
+};
