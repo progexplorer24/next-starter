7ef9edcf2ec42ef322588fd7d9f3e6e685909b59
user <progexplorer@gmail.com>
2020-06-30 10:49:55 +0200

    refactor: prop names for button component

--- components/atoms/button/button.tsx
+++ components/atoms/button/button.tsx
@@ -3,16 +3,16 @@ import { SerializedStyles, css as emotionCss } from "@emotion/core";
 import tw from "twin.macro";
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  css?: SerializedStyles;
+  cssProp?: SerializedStyles;
   className?: string;
   children: ReactNode;
 };
 
-const Button = ({ children, css, ...props }: ButtonProps): ReactElement => {
+const Button = ({ children, cssProp, ...props }: ButtonProps): ReactElement => {
   return (
     <button
       type="button"
-      css={emotionCss([tw`min-w-11 min-h-11`, css])}
+      css={emotionCss([tw`min-w-11 min-h-11`, cssProp])}
       {...props}
     >
       {children}
