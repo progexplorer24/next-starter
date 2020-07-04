1b29ba5885b2a677e26d64b7f3d62f0acde1890a
user <progexplorer@gmail.com>
2020-07-04 12:42:18 +0200

    docs: add documentation for button atoms

--- components/atoms/forms/button/button-submit.tsx
+++ components/atoms/forms/button/button-submit.tsx
@@ -1,19 +1,57 @@
-import React, { ReactNode, ReactElement } from "react";
-import { SerializedStyles, css as emotionCss } from "@emotion/core";
+import React, { ReactElement } from "react";
+import { css as emotionCss } from "@emotion/core";
+import type { ButtonProps } from "@components/atoms/atom-types";
 
-type ButtonSubmitProps = {
-  css?: SerializedStyles;
-  className?: string;
-  children: ReactNode;
-};
-
+/**
+ * # [MDN Documentation Link](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button)
+ *
+ * The HTML <button> element represents a clickable button, used to submit forms or anywhere in a document for accessible, standard button functionality.
+ *
+ * ## Usage Notes
+ *
+ * - `<button>` elements are much easier to style than `<input>` elements. You can add inner HTML content, and
+ * use ::after` and `::before` pseudo-elements for complex rendering.
+ *
+ * - A submit button with the attribute formaction set, but without an associated form does nothing. You have to set a form owner, either by
+ * wrapping it in a <form> or set the attribute form to the id of the form.
+ *
+ * - If your buttons are not for submitting form data to a server, be sure to set their type attribute to button. Otherwise they will try to
+ * submit form data and to load the (nonexistent) response, possibly destroying the current state of the document.
+ *
+ * ## Accessibility Concerns
+ *
+ * ### Icon Buttons
+ *
+ * Buttons that only show an icon to represent do not have an accessible name. Accessible names provide information for assistive technology,
+ * such as screen readers, to access when they parse the document and generate an accessibility tree. Assistive technology then uses the
+ * accessibility tree to navigate and manipulate page content.
+ *
+ * To give an icon button an accessible name, put text in the `<button>` element that concisely describes the button's functionality.
+ *
+ * #### Example
+ *
+ * ```
+ * <button name="favorite">
+ *   <svg aria-hidden="true" viewBox="0 0 10 10"><path d="M7 9L5 8 3 9V6L1 4h3l1-3 1 3h3L7 6z"/></svg>
+ *   Add to favorites
+ * </button>
+ * ```
+ *
+ * ### Size and Proximity
+ *
+ * - A minimum interactive size of **44x44** is recommended.
+ * - Put **spacing** between interactive elements. It will help users with [motor control issues.](https://axesslab.com/hand-tremors/)
+ * - Remember to add focus state to interactive elements.
+ *
+ *
+ */
 const ButtonSubmit = ({
   children,
-  css,
+  cssProp,
   ...props
-}: ButtonSubmitProps): ReactElement => {
+}: Omit<ButtonProps, "type">): ReactElement => {
   return (
-    <button type="submit" css={emotionCss([css])} {...props}>
+    <button type="submit" css={emotionCss([cssProp])} {...props}>
       {children}
     </button>
   );
--- components/atoms/forms/button/button.tsx
+++ components/atoms/forms/button/button.tsx
@@ -3,7 +3,48 @@ import { css as emotionCss } from "@emotion/core";
 import tw from "twin.macro";
 import type { ButtonProps } from "@components/atoms/atom-types";
 
-const Button = ({ children, cssProp, ...props }: ButtonProps): ReactElement => {
+/**
+ * # [MDN Documentation Link](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button)
+ *
+ * The HTML <button> element represents a clickable button, used to submit forms or anywhere in a document for accessible, standard button functionality.
+ *
+ * ## Usage Notes
+ *
+ * - `<button>` elements are much easier to style than `<input>` elements. You can add inner HTML content, and use
+ * `::after` and `::before` pseudo-elements for complex rendering.
+ *
+ * ## Accessibility Concerns
+ *
+ * ### Icon Buttons
+ *
+ * Buttons that only show an icon to represent do not have an accessible name. Accessible names provide information for assistive technology,
+ * such as screen readers, to access when they parse the document and generate an accessibility tree. Assistive technology then uses the
+ * accessibility tree to navigate and manipulate page content.
+ *
+ * To give an icon button an accessible name, put text in the `<button>` element that concisely describes the button's functionality.
+ *
+ * #### Example
+ *
+ * ```
+ * <button name="favorite">
+ *   <svg aria-hidden="true" viewBox="0 0 10 10"><path d="M7 9L5 8 3 9V6L1 4h3l1-3 1 3h3L7 6z"/></svg>
+ *   Add to favorites
+ * </button>
+ * ```
+ *
+ * ### Size and Proximity
+ *
+ * - A minimum interactive size of **44x44** is recommended.
+ * - Put **spacing** between interactive elements. It will help users with [motor control issues.](https://axesslab.com/hand-tremors/)
+ * - Remember to add focus state to interactive elements.
+ *
+ *
+ */
+const Button = ({
+  children,
+  cssProp,
+  ...props
+}: Omit<ButtonProps, "type">): ReactElement => {
   return (
     <button
       type="button"
