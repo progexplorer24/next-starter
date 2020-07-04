1300e4e8f067d0bc7b735ba1edadd6e20df4ac66
user <progexplorer@gmail.com>
2020-07-04 18:09:28 +0200

    refactor: pass svg as prop to component

--- components/atoms/forms/radio-button/radio-button.tsx
+++ components/atoms/forms/radio-button/radio-button.tsx
@@ -1,16 +1,16 @@
 import React, { ReactElement } from "react";
-import { css as emotionCss, SerializedStyles } from "@emotion/core";
+import { css as emotionCss } from "@emotion/core";
 import tw from "twin.macro";
 import addFillColorToSvg from "@utils/add-fill-color-to-svg";
+import type { InputProps } from "@components/atoms/atom-types";
 
-type RadioButtonProps = {
+type RadioButtonProps = Omit<InputProps, "id" | "name"> & {
   name: string;
   id: string;
-  css?: SerializedStyles;
-  className?: string;
+  svg?: JSX.Element;
 };
 
-const svg = (
+const svgIcon = (
   <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
     <circle cx="8" cy="8" r="3" />
   </svg>
@@ -24,17 +24,20 @@ const svg = (
  *
  * ## Usage Notes
  *
- * When you create a URL input with the proper type value, url, you get automatic validation that the entered text is at least in the correct form
- * to potentially be a legitimate URL. This can help avoid cases in which the user mis-types their web site's address, or provides an invalid one.
+ * ## Accessibility Concerns
  *
- * ### Security
+ * ### Providing a bigger hit area for your checkboxes
  *
- * **It's also crucial to remember that a user can tinker with your HTML behind the scenes, so your site must not use this validation for any
- * security purposes. You must verify the URL on the server-side of any transaction in which the provided text may have any security implications
- * of any kind.**
+ * You can toggle a checkbox by clicking on its associated `<label>` element as well as on the checkbox itself. This is a really useful feature
+ * of HTML form labels that makes it easier to click the option you want, especially on small-screen devices like smartphones.
+ *
+ * Beyond accessibility, this is another good reason to properly set up `<label>` elements on your forms.
  *
  */
-const RadioButton = (props: RadioButtonProps): ReactElement => {
+const RadioButton = ({
+  svg = svgIcon,
+  ...props
+}: RadioButtonProps): ReactElement => {
   const radioCheckedStyles = emotionCss`&:checked {
           border-color: transparent;
           background-color: currentColor;
