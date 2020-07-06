e347b6532985867a30da35e71ca29446d31cf497
user <progexplorer@gmail.com>
2020-07-06 15:08:18 +0200

    feat: create announce highlight utility

---
+++ components/atoms/inline-text-semantics/mark/announce-highlight.ts
@@ -0,0 +1,8 @@
+import announceToScreenReader from "@styles/announce-to-screen-reader";
+
+const announceHighlight = announceToScreenReader(
+  "highlight start",
+  "highlight end"
+);
+
+export default announceHighlight;
