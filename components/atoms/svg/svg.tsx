c49f10682fc8b82899d661234aae874d0c797b3b
user <progexplorer@gmail.com>
2020-07-10 09:38:00 +0200

    feat: add defaults to svg atom component

--- components/atoms/svg/svg.tsx
+++ components/atoms/svg/svg.tsx
@@ -3,9 +3,21 @@ import { css as emotionCss } from "@emotion/core";
 import { motion } from "framer-motion";
 import type { SvgProps } from "@components/atoms/atom-types";
 
-const Svg = ({ children, css, ...props }: SvgProps): ReactElement => {
+const Svg = ({
+  children,
+  viewBox = "0 0 24 24",
+  fill = "none",
+  css,
+  ...props
+}: SvgProps): ReactElement => {
   return (
-    <motion.svg {...props} css={emotionCss([css])}>
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={viewBox}
+      fill={fill}
+      {...props}
+      css={emotionCss([css])}
+    >
       {children}
     </motion.svg>
   );
